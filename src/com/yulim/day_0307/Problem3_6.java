2b45993278d9d256818bc0abd581bd8c9f7dccb3
ChoiYuLim <yulim13sky@naver.com>
2023-03-14 17:07:39 +0900

    refactor: 3-6번

--- src/com/yulim/day_0307/Problem3_6.java
+++ src/com/yulim/day_0307/Problem3_6.java
@@ -1,13 +1,17 @@
 package com.yulim.day_0307;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Problem3_6 {
 
     public static void main(String[] args) {
         System.out.println("[숫자 맞추기 게임]");
-        int ans = new java.util.Random().nextInt(10);
+        int ans = new Random().nextInt(10);
         for (int i = 0; i < 5; i++) {
             System.out.println("0 ~ 9 사이의 숫자를 입력 하세요");
-            int num = new java.util.Scanner(System.in).nextInt();
+            Scanner scanner = new Scanner(System.in);
+            int num = scanner.nextInt();
             if (num == ans) {
                 System.out.println("정답!");
                 break;
