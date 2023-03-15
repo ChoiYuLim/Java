3f22b89e9ca927f8bb617ef7778b4ea5343d586d
yulim <yulim13sky@naver.com>
2023-03-15 23:24:02 +0900

    feat: 11장 상속 예시 코드
    0315 super 키워드로 대체

--- src/com/yulim/day_0315/Example11/SuperHero.java
+++ src/com/yulim/day_0315/Example11/SuperHero.java
@@ -24,9 +24,9 @@ public class SuperHero extends Hero {
 
 	@Override
 	public void attack(Kinoko enemy) {
-		System.out.println(this.getName() + "의 공격!");
-		enemy.hp -= 5;
-		System.out.println("5포인트의 데미지를 주었다!");
+		
+		// 부모 객체를 참조하는 super 키워드
+		super.attack(enemy);
 
 		if (this.flying) {
 			System.out.println(this.getName() + "의 공격!");
