0051d7b48f5334f25dc43e237a2bb87f89a73f46
yulim <yulim13sky@naver.com>
2023-03-15 23:53:27 +0900

    feat: 11장 상속 예시 코드
    0315 기본 생성자가 없을 때

---
+++ src/com/yulim/day_0315/Example11/Main2.java
@@ -0,0 +1,8 @@
+package com.yulim.day_0315.Example11;
+
+public class Main2 {
+	public static void main(String[] args) {
+		Weapon weapon1 = new Weapon("gun");
+		Weapon weapon2 = new Weapon("gun2");
+	}
+}
---
+++ src/com/yulim/day_0315/Example11/Weapon.java
@@ -0,0 +1,11 @@
+package com.yulim.day_0315.Example11;
+
+public class Weapon extends Item {
+	public Weapon(String name) {
+		super(name);
+	}
+
+	public Weapon(String name, int price) {
+		super(name, price);
+	}
+}
