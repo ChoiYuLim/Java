58df902db3f0c38b7c537771d8b1bbdcefb3f43b
ChoiYuLim <yulim13sky@naver.com>
2023-03-15 15:32:40 +0900

    feat: 0315 12-1번

---
+++ src/com/yulim/day_0315/Problem12/Computer.java
@@ -0,0 +1,15 @@
+package com.yulim.day_0315.Problem12;
+
+public class Computer extends TangibleAsset {
+    private String makerName;
+
+    public Computer(String name, int price, String color, String makerName) {
+        super(name, price, color);
+        this.makerName = makerName;
+    }
+
+    public String getMakerName() {
+        return makerName;
+    }
+
+}
---
+++ src/com/yulim/day_0315/Problem12/Main.java
@@ -0,0 +1,10 @@
+package com.yulim.day_0315.Problem12;
+
+public class Main {
+    public static void main(String[] args) {
+        Computer computer = new Computer("울트라 갤럭시", 2300000, "블랙", "삼성");
+        Book book = new Book("데미안",13000, "흰색","bn");
+
+        System.out.println(computer.getName());
+    }
+}
