eb2a8ae1e286d72b96e80cd1a6d353791c56cd56
yulim <yulim13sky@naver.com>
2023-03-15 22:48:44 +0900

    feat: 11장 상속 예시 코드
    0315

---
+++ src/com/yulim/day_0315/Example11/Main.java
@@ -0,0 +1,9 @@
+package com.yulim.day_0315.Example11;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SuperHero superHero = new SuperHero();
+		superHero.run();
+	}
+}
--- src/com/yulim/day_0315/Example11/SuperHero.java
+++ src/com/yulim/day_0315/Example11/SuperHero.java
@@ -1,29 +1,17 @@
 package com.yulim.day_0315.Example11;
 
-public class SuperHero {
-	private String name = "최유림";
-	private int hp = 100;
-	private boolean flying; // 필드 추가
+// Hero 클래스를 상속한 SuperHero
+public class SuperHero extends Hero{
+	
+	private boolean flying; // 추가한 필드
 
-	// 싸우기
-	public void attack(Kinoko enemy) {
-		System.out.println(this.name + "의 공격!");
-		this.hp -= 5;
-		System.out.println("5포인트의 데미지를 주었다!");
-	}
-
-	// 도망
-	public void run() {
-		System.out.println(this.name + "는 도망쳤다!");
-	}
-
-	// 날기
+	// 추가한 메소드
 	public void fly() {
 		flying = true;
 		System.out.println("날았다!");
 	}
 
-	// 착지
+	// 추가한 메소드
 	public void land() {
 		flying = false;
 		System.out.println("착지했다!");
