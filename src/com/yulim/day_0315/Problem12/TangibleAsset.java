751480f693acbb86e039067712a858f9e06dd808
ChoiYuLim <yulim13sky@naver.com>
2023-03-15 17:03:47 +0900

    feat: 0315 12-4번

--- src/com/yulim/day_0315/Problem12/TangibleAsset.java
+++ src/com/yulim/day_0315/Problem12/TangibleAsset.java
@@ -1,8 +1,21 @@
 package com.yulim.day_0315.Problem12;
 
-public abstract class TangibleAsset extends Asset {
+public abstract class TangibleAsset extends Asset implements Thing {
+
+    private double weight;
+
     public TangibleAsset(String name, int price, String color) {
         super(name, price, color);
     }
     
+    @Override
+    public double getWeight() {
+        return weight;
+    }
+
+    @Override
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
 }
