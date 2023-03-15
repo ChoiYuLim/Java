10fd276bead7919733fc14f07e294fcf3dd8a62a
yulim <yulim13sky@naver.com>
2023-03-16 02:21:21 +0900

    refactor: 12장 문제 main 수정
    0315

--- src/com/yulim/day_0315/Problem12/Main.java
+++ src/com/yulim/day_0315/Problem12/Main.java
@@ -1,10 +1,14 @@
 package com.yulim.day_0315.Problem12;
 
 public class Main {
-    public static void main(String[] args) {
-        Computer computer = new Computer("울트라 갤럭시", 2300000, "블랙", "삼성");
-        Book book = new Book("데미안",13000, "흰색","bn");
-
-        System.out.println(computer.getName());
-    }
+	public static void main(String[] args) {
+		Computer computer = new Computer("울트라 갤럭시", 2300000, "블랙", "삼성");
+		Book book = new Book("데미안", 13000, "흰색", "bn");
+		computer.setWeight(1.5);
+		System.out.println(computer.getName());
+		System.out.println(computer.getPrice());
+		System.out.println(computer.getColor());
+		System.out.println(computer.getMakerName());
+		System.out.println(computer.getWeight());
+	}
 }
