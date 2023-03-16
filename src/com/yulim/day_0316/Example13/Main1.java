d21b39b6f3fec2ec5a21431dccbafff3b0298352
ChoiYuLim <yulim13sky@naver.com>
2023-03-16 18:51:01 +0900

    fix: attack 인자 오류 수정
    0316 오류 수정 attack 인자

--- src/com/yulim/day_0316/Example13/Main1.java
+++ src/com/yulim/day_0316/Example13/Main1.java
@@ -5,10 +5,9 @@ public class Main1 {
     public static void main(String[] args) {
         Wizard w = new Wizard();
         Kinoko k = new Kinoko();
+        Monster m = new Monster();
         w.name = "레이나";
-        w.attack(k);
+        w.attack(m);
         w.fireball(k);
-
     }
-
 }
