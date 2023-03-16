fdcb5d71ce2dd11d45a1606f9b5a75eadd227a22
ChoiYuLim <yulim13sky@naver.com>
2023-03-16 19:10:00 +0900

    feat: 14장 예시 코드
    0316 임시

---
+++ src/com/yulim/day_0316/application/Example3/Main2.java
@@ -0,0 +1,39 @@
+package com.yulim.day_0316.application.Example3;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main2 {
+    public static void main(String[] args) {
+        List<String> strs = new ArrayList<>();
+        strs.add("이동학");
+        strs.add("박경덕");
+        strs.add("강태근");
+        strs.add("최유림");
+
+        Collections.sort(strs);
+        System.out.println(strs);
+        
+        List<Integer> nums = new ArrayList<>();
+        nums.add(5);
+        nums.add(1);
+        nums.add(8);
+        nums.add(2);
+        Collections.sort(nums);
+        System.out.println(nums);
+        
+        
+        List<Student> students = new ArrayList<>();
+
+        students.add(new Student("이동학", 0));
+        students.add(new Student("최유림", 3));
+        students.add(new Student("박경덕", 1));
+        students.add(new Student("강태근", 2));
+        
+
+        Collections.sort(students); // 내가 만든 건 무슨 순서인지 모르기 때문에 오류남
+        System.out.println(students);
+    }
+
+}
---
+++ src/com/yulim/day_0316/application/Example3/Student.java
@@ -0,0 +1,93 @@
+package com.yulim.day_0316.application.Example3;
+
+import java.util.Objects;
+
+public class Student implements Comparable<Student> {
+
+    public Student(String name, int id) {
+        this.name = name;
+    }
+
+    private String name;
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [name=" + name + ", id=" + id + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Student other = (Student) obj;
+        return id == other.id;
+    }
+
+    // implements 했을 때 오버라이딩 해야할 것 compare to
+    @Override
+    public int compareTo(Student o) {
+        // return (id < o.id) ? -1 : ((id == o.id) ? 0 : 1); // id순 정렬
+        return this.name.compareTo(o.name); // 이름 순으로 정렬
+        // return this.name.compareTo(o.name) * -1; //이름 순으로 역순으로 정렬
+
+    }
+
+    
+    // 근데 만약에 정렬 기준을 바꾸고 싶으면, 이렇게 안함
+    // 여기서 하나 정해놓고 이렇게 안 씀
+
+
+    // @Override
+    // public String toString() {
+    // return "Student [name=" + name + "]";
+    // }
+    //
+    // // 굳이 하나를 빼놓지 않고 거의 대부분 hashcode와 equals를 모두 재정의함
+    //
+    // @Override
+    // public int hashCode() {
+    // return Objects.hash(name);
+    // }
+    //
+    // @Override
+    // public boolean equals(Object obj) {
+    // if (this == obj)
+    // return true;
+    // if (obj == null)
+    // return false;
+    // if (getClass() != obj.getClass())
+    // return false;
+    // Student other = (Student) obj;
+    // return Objects.equals(name, other.name);
+    // }
+
+
+
+}
