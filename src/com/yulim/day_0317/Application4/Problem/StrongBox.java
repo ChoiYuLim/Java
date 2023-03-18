76fa3c57dbc0fa6f3434bd068f1ef287780a9090
yulim <yulim13sky@naver.com>
2023-03-19 02:59:24 +0900

    feat: 응용 4장 연습 문제 4-2
    0317

--- src/com/yulim/day_0317/Application4/Problem/StrongBox.java
+++ src/com/yulim/day_0317/Application4/Problem/StrongBox.java
@@ -2,17 +2,49 @@ package com.yulim.day_0317.Application4.Problem;
 
 public class StrongBox<E> {
 
-	// 금고 클래스에 담는 인스턴스의 타입은 미정
 	private E thing;
+	private KeyType type;
+	private int count;
 
-	// put 메서드로 인스턴스를 저장
+	public StrongBox(KeyType type) {
+		this.type = type;
+		this.count = 0;
+	}
+
 	public void put(E thing) {
 		this.thing = thing;
 	}
 
-	// get 메서드로 인스턴스를 얻을 수 있음
 	public E get() {
-		return this.thing;
+		count++;
+		if (count <= this.type.getValue()) {
+			return null;
+		} else {
+			return this.thing;
+		}
 	}
 
+	enum KeyType {
+		PADLOCK(1024), BUTTON(10000), DIAL(30000), FINGER(1000000);
+
+		private final int value;
+
+		KeyType(int value) {
+			this.value = value;
+		}
+
+		public int getValue() {
+			return value;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		StrongBox sb = new StrongBox(KeyType.PADLOCK);
+		sb.put("money");
+		for (int i = 0; i < 2000; i++) {
+			System.out.println(sb.get());
+		}
+
+	}
 }
