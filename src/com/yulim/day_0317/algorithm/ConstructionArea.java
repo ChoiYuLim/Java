2f70870aee2e980cfb9b6c9c811dbd12133b35cd
yulim <yulim13sky@naver.com>
2023-03-20 02:50:04 +0900

    feat: 조용한 곳 찾기 ConstructionArea class
    0317

---
+++ src/com/yulim/day_0317/algorithm/ConstructionArea.java
@@ -0,0 +1,25 @@
+package com.yulim.day_0317.algorithm;
+
+public class ConstructionArea {
+	private int x;
+	private int y;
+	private int noise;
+
+	ConstructionArea(int x, int y, int noise) {
+		this.x = x;
+		this.y = y;
+		this.noise = noise;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public int getNoise() {
+		return noise;
+	}
+}
