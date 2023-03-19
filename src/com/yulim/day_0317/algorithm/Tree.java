7ad0602f908bb015b9c26766740c6345ebac65db
yulim <yulim13sky@naver.com>
2023-03-20 02:48:15 +0900

    feat: 조용한 곳 찾기 Tree class
    0317

---
+++ src/com/yulim/day_0317/algorithm/Tree.java
@@ -0,0 +1,30 @@
+package com.yulim.day_0317.algorithm;
+
+public class Tree {
+
+	private int x;
+	private int y;
+	private int count;
+
+	Tree(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+}
