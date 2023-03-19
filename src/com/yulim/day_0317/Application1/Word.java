8b9280ebf606f773ce5c0b58afd458f9804100f2
yulim <yulim13sky@naver.com>
2023-03-20 01:52:26 +0900

    feat: 응용 1장 퀴즈 1 모음
    0317

---
+++ src/com/yulim/day_0317/Application1/Word.java
@@ -0,0 +1,18 @@
+package com.yulim.day_0317.Application1;
+
+public class Word {
+	private String letters;
+
+	public Word(String letters) {
+		this.letters = letters;
+	}
+
+	public boolean isVowel(int i) {
+		return "aioueAIOUE".contains(letters.substring(i, i + 1));
+	}
+
+	public static void main(String[] args) {
+		Word w = new Word("ajlk");
+		System.out.println(w.isVowel(0));
+	}
+}
