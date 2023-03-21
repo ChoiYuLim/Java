01d22e9e956e336691ed3fda1a1106ad300c5a04
yulim <yulim13sky@naver.com>
2023-03-21 09:20:03 +0900

    refactor: 6-1 수정
    0320

--- src/com/yulim/day_0320/Application6/Problem/Problem6_1.java
+++ src/com/yulim/day_0320/Application6/Problem/Problem6_1.java
@@ -9,7 +9,7 @@ public class Problem6_1 {
             FileOutputStream fileOutput = new FileOutputStream(args[1]);
        
             // word의 값을 byte로 변환하여 저장
-            byte b[] = fileInput.readAllBytes();
+            byte[] b = fileInput.readAllBytes();
 
             // 파일에 word 작성하기
             fileOutput.write(b);
