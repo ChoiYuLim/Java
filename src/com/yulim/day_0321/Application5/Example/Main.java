f3cc26f434fa79f9c1ad17a848617855634553e4
yulim <yulim13sky@naver.com>
2023-03-21 22:40:34 +0900

    feat: 5장 메서드를 변수에 대입하여 사용 예시 코드
    0321

--- src/com/yulim/day_0321/Application5/Example/Main.java
+++ src/com/yulim/day_0321/Application5/Example/Main.java
@@ -3,42 +3,15 @@ package com.yulim.day_0321.Application5.Example;
 import java.util.function.IntBinaryOperator;
 
 public class Main {
-    public static int add(int x, int y) {
-        return x + y;
-    }
+	public static int add(int x, int y) {
+		return x + y;
+	}
 
-    public static void main(String[] args) {
-        IntBinaryOperator func = Main::add;
-        
-        int result = func.applyAsInt(5,3);
-        System.out.println("5 + 3 = " + result);
-    }
-//    interface MyFunction {
-//        int call(int a, int b);
-//    }
-//
-//    public static int add(int a, int b) {
-//        return a + b;
-//    }
-//
-//    public static void main(String[] args) {
-//        IntBinaryOperator addFunction = Main::add;
-//        MyFunction deohagiFunction = Main::add;
-//
-//        // lamda expression
-//        MyFunction addFunction2 = (int a, int b) -> {
-//            return a + b;
-//        }; // 익명 함수라고 볼 수 있음
-//           // 그때 그때 필요한거 바로 남둚
-//
-//        MyFunction gophagi = (a, b) -> {
-//            return a * b;
-//        };
-//
-//        MyFunction nanugi = (a, b) -> a / b;
-//
-//        int result = addFunction.applyAsInt(2, 3);
-//
-//        result = deohagiFunction.call(3, 4);
-//    }
+	public static void main(String[] args) {
+		IntBinaryOperator func = Main::add;
+
+		int result = func.applyAsInt(5, 3);
+		System.out.println("5 + 3 = " + result);
+	}
+
 }
