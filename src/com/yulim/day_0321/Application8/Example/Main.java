0323e71e2fd09a828b0fbd54b82f8313e373df31
yulim <yulim13sky@naver.com>
2023-03-21 23:57:03 +0900

    feat: 응용 8장 예제 코드
    0321

--- src/com/yulim/day_0321/Application8/Example/Main.java
+++ src/com/yulim/day_0321/Application8/Example/Main.java
@@ -5,16 +5,21 @@ import java.io.InputStreamReader;
 import java.net.URL;
 
 public class Main {
-    public static void main(String[] args) {
-//        URL url = new URL("http://www.google.com");
-//        InputStream is = url.openStream();
-//        InputStreamReader isr = new InputStreamReader(is);
-//        int i = isr.read();
-//        while (i != -1) {
-//            System.out.println((char) i);
-//            i = isr.read();
-//        }
-//        isr.close();
-        
-    }
+	public static void main(String[] args) {
+		try {
+			URL url = new URL("http://www.google.com");
+			// InputStream: 바이트 단위로 데이터를 읽어들이는 스트림 클래스
+			// URL.openStream(): 주어진 URL에서 데이터를 읽어들이기 위한 메서드
+			InputStream is = url.openStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			int i = isr.read();
+			while (i != -1) {
+				System.out.println((char) i);
+				i = isr.read();
+			}
+			isr.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
