97a59d59e3656a1ed0a39d75cef38503d8e484cc
yulim <yulim13sky@naver.com>
2023-03-22 22:51:55 +0900

    feat: 응용 11장 counter 동기화 수행한 예시 코드
    0322

--- src/com/yulim/day_0322/Application11/Example/Counter.java
+++ src/com/yulim/day_0322/Application11/Example/Counter.java
@@ -1,22 +1,24 @@
 package com.yulim.day_0322.Application11.Example;
 
 public class Counter {
-    int num = 0;
+	int num = 0;
 
-    public static void main(String[] args) throws Exception {
-        Counter counter = new Counter();
+	public static void main(String[] args) throws Exception {
+		Counter counter = new Counter();
 
-        for (int i = 0; i < 1000; i++) { // 1000개한테 실행해! 하고 for문은 끝났지만, 안에 있는 thread들은 아직 일을 하는 중
-            new Thread(() -> {
-                try {
-                    Thread.sleep(10);
-                    counter.num++;
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }).start();
-        }
-        Thread.sleep(5000);
-        System.out.println(counter.num);
-    }
+		for (int i = 0; i < 1000; i++) { // 1000개한테 실행해! 하고 for문은 끝났지만, 안에 있는 thread들은 아직 일을 하는 중
+			new Thread(() -> {
+				try {
+					Thread.sleep(10);
+					synchronized (counter) {
+						counter.num++;
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}).start();
+		}
+		Thread.sleep(5000);
+		System.out.println(counter.num);
+	}
 }
