be954dc8b34313f0c67fe27016ce924018247b60
yulim <yulim13sky@naver.com>
2023-03-27 03:14:01 +0900

    refactor: Loan 클래스
    0327

--- src/com/yulim/day_0323/finalProject/LoanManager.java
+++ src/com/yulim/day_0323/finalProject/LoanManager.java
@@ -4,30 +4,53 @@ import java.util.ArrayList;
 
 public class LoanManager {
 
-    ArrayList<Loan> list;
+    ArrayList<Loan> list = new ArrayList<>();
 
     public ArrayList<Loan> getList() {
         return list;
     }
 
-    // 특정 회원의 대출 이력 전체 조회
-    public void readLoanHistory(int memeberId) {
-        // TODO Auto-generated method stub
+    // 책 id, 회원 id로 현재 빌리고 있는 책 찾기
+    public Loan findLoan(int bookId, int memberId) {
+        return list.stream().filter(i -> i.getIsReturned() == false && i.getBookId() == bookId
+                && i.getMemberId() == memberId).toList().get(0);
+    }
 
+    // 특정 회원의 모든 대출 이력
+    public void readAllBook(int memberId) {
+        if (list.size() == 0) {
+            System.out.println("대출 이력이 없습니다.");
+        }
+        for (Loan loan : list) {
+            if (loan.getMemberId() == memberId) {
+                System.out.println(loan);
+            }
+        }
+    }
+
+    // 특정 회원의 현재 대출 중인 책
+    public void readNowBook(int memberId) {
+        for (Loan loan : list) {
+            if (loan.getMemberId() == memberId && loan.getIsReturned() == false) {
+                System.out.println(loan);
+            }
+        }
     }
 
     // 책 대출
-    public void loanBook(int memberId, int bookId) {
-
+    public void loanBook(Loan loan) {
+        list.add(loan);
+        loan.setIsReturned(false);
     }
 
     // 책 반납
-    public void returnBook(int bookId) {
-
+    public void returnBook(Loan loan) {
+        loan.setIsReturned(true);
     }
 
     // 반납 연기
-    public void extendBook(int bookId) {
-
+    public void extendBook(Loan loan) {
+        loan.setIsExtended(true);
+        loan.setDeadLine(Util.addDate(loan.getDeadLine(), 7));
     }
 }
